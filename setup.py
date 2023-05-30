d785845fcd8742ee115813c5593235acb47824c3
nacho-herrera <65458529+nacho-herrera@users.noreply.github.com>
2023-05-30 16:21:14 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyCocos",
+    version="0.2.0",
+    author="Nacho Herrera",
+    author_email="github@nachoherrera.com.ar",
+    description="Python connector for Cocos Capital's Rest APIs.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/nacho-herrera/pyCocos",
+    packages=setuptools.find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'requests>=2.28.2',
+        'simplejson>=3.19.1',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "Topic :: Software Development"
+    ],
+)
