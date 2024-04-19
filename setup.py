4035db7adf2df8bc6cbda6c81ea6adab5edc4c47
nacho-herrera <65458529+nacho-herrera@users.noreply.github.com>
2024-04-19 01:11:57 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyCocos",
-    version="0.2.6",
+    version="0.2.7",
     author="Nacho Herrera",
     author_email="github@nachoherrera.com.ar",
     description="Python connector for Cocos Capital's Rest APIs.",
